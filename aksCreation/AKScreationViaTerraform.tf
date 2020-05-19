7e79db14d8f2c7efa2cca325cc7d30152fdc59d0
SGPhilip <53293805+SGPhilip@users.noreply.github.com>
2020-05-19 13:09:13 +0530

    Create AKScreationViaTerraform.tf

---
+++ aksCreation/AKScreationViaTerraform.tf
@@ -0,0 +1,76 @@
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "randstad-tfstate-rg"
+    storage_account_name = "randstadtf9iydj9nc"
+    container_name       = "core-tfstate"
+    key                  = "test.AKS.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  version = "2.9.0"
+  features {}
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "nginxingress"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "nginxcluster"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "nginxapp"
+  
+  default_node_pool {
+            
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+    enable_auto_scaling = "true"
+    min_count = 1
+    max_count = 10 
+  }
+
+  linux_profile {
+        admin_username = "ubuntu"
+
+        ssh_key {
+            key_data = file(var.ssh_public_key)
+        }
+
+  }
+  
+  role_based_access_control {
+          enabled = true
+  }
+
+   addon_profile {
+    http_application_routing {
+      enabled = true
+    }
+  }      
+  identity {
+    type = "SystemAssigned"
+  }
+
+  
+  network_profile {
+    network_plugin = "azure"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+}
