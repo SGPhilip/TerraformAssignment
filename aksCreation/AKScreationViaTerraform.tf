0fac57eaf8efd703e8501cc3bacde6f155d30a97
SGPhilip <53293805+SGPhilip@users.noreply.github.com>
2020-05-20 07:36:39 +0530

    Update AKScreationViaTerraform.tf

--- aksCreation/AKScreationViaTerraform.tf
+++ aksCreation/AKScreationViaTerraform.tf
@@ -16,15 +16,15 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "example" {
-  name     = "nginxingressviapipeline"
+  name     = "myAksTerraformResourcegroup"
   location = "West Europe"
 }
 
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "nginxclusterviapipeline"
+  name                = "myAksTerraformCluster"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "nginxapp"
+  dns_prefix          = "aksterraformcluster"
   
   default_node_pool {
             
