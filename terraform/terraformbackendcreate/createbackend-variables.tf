6a51cad2050182b7fc3b6a6ab92127f0a3809c01
SGPhilip <53293805+SGPhilip@users.noreply.github.com>
2020-05-24 13:18:05 +0530

    Update createbackend-variables.tf

--- terraform/terraformbackendcreate/createbackend-variables.tf
+++ terraform/terraformbackendcreate/createbackend-variables.tf
@@ -1,1 +1,17 @@
-.
+variable "company" {
+  type = string
+  description = "This variable defines the name of the company"
+  default = "Randstad"
+}
+# environment
+variable "environment" {
+  type = string
+  description = "This variable defines the environment to be built"
+  default = "Test"
+}
+# azure region
+variable "location" {
+  type = string
+  description = "Azure region where resources will be created"
+  default = "West Europe"
+}
